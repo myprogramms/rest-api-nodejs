d21e7d270f5c9ac53131300ee49d2359208a9bc1
awsprepaid2@mml.local-at-867636236097 <awsprepaid2@mml.local-at-867636236097>
2021-07-11 11:13:57 +0530

    added image

---
+++ README.txt
@@ -0,0 +1,8 @@
+npm init
+npm install nodemon -g
+npm i express
+
+
+//create server.js file.
+nodemon server.js
+
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const server = express();
 
-server.get('/',function(request,response){
-     console.log(request.query.query1);
-    console.log(request.quesry);
+server.get('/', function (request, response) {
+    console.log(request.query.query1);
+    // console.log(request.query);
     console.log(request.query.param2);
-    response.send('Hello World');
+    //----- READ headers----
+    console.log(request.headers);
+    response.send('Hello world');
 })
 
-server.listen(3000,function(){
+server.listen(3000, function () {
     console.log('server started..');
 })
 
